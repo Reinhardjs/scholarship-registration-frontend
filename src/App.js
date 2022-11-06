dec742ffb76a4ba2467698cc3443c229f09b6ad9
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-06 23:41:29 +0700

    added changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, World!
+          This is the frontend app!
         </p>
         <a
           className="App-link"
