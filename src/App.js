90bce49081e4e055fb3139ebad09c82de3cbb265
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-20 02:37:09 +0700

    chore(mext-registration): added landing page & implement router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import JapaneseStudiesRegistration from "./pages/JapaneseStudiesRegistration";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/studies">
-          <JapaneseStudiesRegistration />
-        </Route>
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/">
+        <LandingPage />
+      </Route>
+      <Route path="/studies">
+        <JapaneseStudiesRegistration />
+      </Route>
+    </Switch>
   );
 }
 
