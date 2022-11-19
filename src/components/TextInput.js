0cc89da7c60e2fdbb0119634d78d13be6465085d
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-20 02:06:59 +0700

    chore(mext-registration): added complete inputs for students form

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TextInput = (props) => {
-  const { form, labelString, inputName, placeholder } = props;
+  const { form, labelString, inputName, placeholder, validation } = props;
   const {
     register,
     formState: { errors },
@@ -9,15 +9,20 @@ const TextInput = (props) => {
 
   return (
     <div className="text-input-container">
-      <label class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+      <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
         {labelString}
       </label>
       <input
-        class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
         placeholder={placeholder}
-        {...register(inputName, { required: true })}
+        {...register(inputName, { required: true, validate: validation })}
       />
-      {errors[inputName] && <span>Silakan isi {labelString} Anda</span>}
+      {errors[inputName] &&
+        (errors[inputName].type === "validate" ? (
+          <span>{labelString} tidak valid</span>
+        ) : (
+          <span>Silakan isi {labelString}</span>
+        ))}
     </div>
   );
 };
--- src/pages/JapaneseStudiesRegistration.js
+++ src/pages/JapaneseStudiesRegistration.js
@@ -6,29 +6,97 @@ const JapaneseStudiesRegistration = () => {
   const form = useForm();
   const { handleSubmit } = form;
 
+  const isValidEmail = (email) =>
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+
+  const handleEmailValidation = (email) => {
+    return isValidEmail(email);
+  };
+
   const onSubmit = (data) => console.log(data);
 
   return (
     <React.Fragment>
-      <div class="grid h-screen place-items-center">
-        <div class="min-w-full">
+      <div className="grid h-screen place-items-center">
+        <div className="min-w-full">
           <form
-            className="m-auto max-w-2xl border py-10 px-12"
+            className="m-auto mt-10 mb-10 max-w-2xl rounded-lg border bg-white py-10 px-12 shadow"
             onSubmit={handleSubmit(onSubmit)}
           >
-            <div class="space-y-4 p-2 sm:p-4 md:space-y-6">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
+            <div className="space-y-4 p-2 sm:p-4 md:space-y-6">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
                 Students Registration
               </h1>
               <TextInput form={form} labelString={"Nama"} inputName={"name"} />
               <TextInput
                 form={form}
+                labelString={"Alamat"}
+                inputName={"address"}
+              />
+              <TextInput
+                form={form}
+                labelString={"Telepon"}
+                inputName={"telephone"}
+              />
+              <TextInput
+                form={form}
+                labelString={"Handphone"}
+                inputName={"handphone"}
+              />
+              <TextInput
+                form={form}
+                labelString={"Jenis Kelamin"}
+                inputName={"gender"}
+              />
+              <TextInput
+                form={form}
+                labelString={"Tanggal Lahir"}
+                inputName={"birthdate"}
+              />
+              <TextInput
+                form={form}
                 labelString={"Email"}
                 inputName={"email"}
+                validation={handleEmailValidation}
               />
-              <div class="pt-2.5">
+              <TextInput
+                form={form}
+                labelString={"Japanese Resident?"}
+                inputName={"japaneseResident"}
+              />
+              <TextInput
+                form={form}
+                labelString={"Provinsi"}
+                inputName={"province"}
+              />
+              <TextInput form={form} labelString={"Kota"} inputName={"city"} />
+              <TextInput
+                form={form}
+                labelString={"Universitas"}
+                inputName={"university"}
+              />
+              <TextInput
+                form={form}
+                labelString={"Semester"}
+                inputName={"semester"}
+              />
+              <TextInput form={form} labelString={"IPK"} inputName={"ipk"} />
+              <TextInput form={form} labelString={"JLPT"} inputName={"jlpt"} />
+              <TextInput
+                form={form}
+                labelString={"JLPT Score"}
+                inputName={"jlptScore"}
+              />
+              <TextInput
+                form={form}
+                labelString={"Lokasi Test"}
+                inputName={"testLocation"}
+              />
+              <div className="pt-2.5">
                 <button
-                  class="w-full rounded-lg bg-primary-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  className="w-full rounded-lg bg-primary-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                   type="submit"
                 >
                   Submit
