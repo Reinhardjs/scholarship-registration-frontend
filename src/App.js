ad66215bf8e4e4c6d5dc45c6f61b4a1933ce4f79
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-19 23:18:12 +0700

    chore(mext-registration): use react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
 import JapaneseStudiesRegistration from "./pages/JapaneseStudiesRegistration";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <JapaneseStudiesRegistration />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/studies">
+          <JapaneseStudiesRegistration />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
