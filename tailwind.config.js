058bba1d267de186fba7dfa3d3d08c4005fa6d13
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-20 17:41:27 +0700

    chore(mext-registration): modify tailwind.config.js to s support flowbite-datepicker

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  plugins: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-datepicker/**/*.{js,jsx,ts,tsx}",
+  ],
+  plugins: [require("flowbite/plugin")],
   darkMode: "class",
   theme: {
     extend: {
