e65e805bf24640885d73d7f8c028296f2dfc0d5e
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-20 20:22:59 +0700

    chore(mext-registration): restyling phone number input focus border color, by using gray:900

--- src/components/PhoneNumberInput.js
+++ src/components/PhoneNumberInput.js
@@ -18,7 +18,7 @@ const PhoneNumberInput = (props) => {
         </span>
         <input
           type="number"
-          class="block w-full min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 [appearance:textfield] focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          class="block w-full min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 [appearance:textfield] focus:border-gray-900 focus:ring-gray-900  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-gray-900 dark:focus:ring-gray-900"
           placeholder={placeholder}
           {...register(inputName, { required: true, validate: validation })}
         />
