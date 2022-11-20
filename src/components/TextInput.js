bafd4841e648e47fc4af495364fce4c310b26b87
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-20 20:24:54 +0700

    chore(mext-registration): apply border-gray-900 to all text input component's on focus style

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -20,7 +20,7 @@ const TextInput = (props) => {
         {labelString}
       </label>
       <input
-        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-gray-900 focus:ring-gray-900  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-gray-900 dark:focus:ring-gray-900 sm:text-sm"
         placeholder={placeholder}
         {...register(inputName, { required: isRequired, validate: validation })}
       />
