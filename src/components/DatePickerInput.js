30978de2aab06b1e0e0844d9b5f82b5e1bfc694a
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-20 20:22:35 +0700

    chore(mext-registration): restyling date picker input to be same as phone number input

--- src/components/DatePickerInput.js
+++ src/components/DatePickerInput.js
@@ -21,8 +21,8 @@ const DatePickerInput = (props) => {
       <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
         {labelString}
       </label>
-      <div class="relative">
-        <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+      <div class="flex">
+        <span class="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400">
           <svg
             aria-hidden="true"
             class="h-5 w-5 text-gray-500 dark:text-gray-400"
@@ -36,12 +36,12 @@ const DatePickerInput = (props) => {
               clip-rule="evenodd"
             ></path>
           </svg>
-        </div>
+        </span>
         <input
           id="datePicker"
           {...register(inputName, { required: true })}
           defaultValue=""
-          class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          class="block w-full min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 [appearance:textfield] focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
           placeholder={"Pilih " + labelString}
         />
       </div>
