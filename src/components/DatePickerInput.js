6be4f2c341984879d28157812ce9180bd0489b36
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-27 17:29:17 +0700

    chore(mext-registration): use className instead of class

--- src/components/DatePickerInput.js
+++ src/components/DatePickerInput.js
@@ -21,11 +21,11 @@ const DatePickerInput = (props) => {
       <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
         {labelString}
       </label>
-      <div class="flex">
-        <span class="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400">
+      <div className="flex">
+        <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400">
           <svg
             aria-hidden="true"
-            class="h-5 w-5 text-gray-500 dark:text-gray-400"
+            className="h-5 w-5 text-gray-500 dark:text-gray-400"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +41,7 @@ const DatePickerInput = (props) => {
           id="datePicker"
           {...register(inputName, { required: true })}
           defaultValue=""
-          class="block w-full min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 [appearance:textfield] focus:border-gray-900 focus:ring-gray-900  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-gray-900 dark:focus:ring-gray-900 sm:text-sm"
+          className="block w-full min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 [appearance:textfield] focus:border-gray-900 focus:ring-gray-900  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-gray-900 dark:focus:ring-gray-900 sm:text-sm"
           placeholder={"Pilih " + labelString}
         />
       </div>
--- src/components/PhoneNumberInput.js
+++ src/components/PhoneNumberInput.js
@@ -14,13 +14,13 @@ const PhoneNumberInput = (props) => {
       <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
         {labelString}
       </label>
-      <div class="flex">
-        <span class="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400">
+      <div className="flex">
+        <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400">
           +62
         </span>
         <input
           type="number"
-          class="block w-full min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 [appearance:textfield] focus:border-gray-900 focus:ring-gray-900  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-gray-900 dark:focus:ring-gray-900 sm:text-sm"
+          className="block w-full min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 [appearance:textfield] focus:border-gray-900 focus:ring-gray-900  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-gray-900 dark:focus:ring-gray-900 sm:text-sm"
           placeholder={placeholder}
           onWheel={(e) => e.target.blur()}
           value={phoneNumber}
