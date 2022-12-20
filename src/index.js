196786de4917ca418a9ed800d61bfdfc695ca3b9
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-20 09:43:49 +0700

    chore(mext-registration): fix typo

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-ReactDOM.render(<App />, document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
